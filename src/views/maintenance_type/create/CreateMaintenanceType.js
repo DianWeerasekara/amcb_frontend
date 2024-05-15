03ec012162ab2b59c32b402b503c502f569b170e
DianWeerasekara <dianskyline@gmail.com>
2024-05-15 13:47:25 +0300

    Update CreateMaintenanceType.js

--- src/views/maintenance_type/create/CreateMaintenanceType.js
+++ src/views/maintenance_type/create/CreateMaintenanceType.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { CRow, CCol, CFormInput, CContainer, CCard, CCardBody, CFormSelect, CButton, CFormTextarea } from "@coreui/react";
+import { CRow, CCol, CFormInput, CContainer, CCard, CCardBody, CFormSelect, CButton, CFormTextarea, CAlert } from "@coreui/react";
+import axios from "axios";
 
 const CreateMaintenanceType = () => {
     
@@ -11,10 +12,14 @@ const CreateMaintenanceType = () => {
     const [maintenanceDescription, setMaintenanceDescription] = useState('');
     const [componentsInvolved, setComponentsInvolved] = useState('');
 
+    const [error, setError] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+
     useEffect(() => {
         fetchLineTypes();
     }, []);
 
+    //get request for the line types
     const fetchLineTypes = async () => {
         try {
             const response = await fetch("http://localhost:9000/api/line-type");
@@ -35,6 +40,34 @@ const CreateMaintenanceType = () => {
     const handleLineTypeChange = (event) => {
         setSelectedLineType(event.target.value);
     };
+
+    const handleSave = () => {
+        const regMaintenanceType = {
+            name: maintenanceTypeName,
+            linetype: selectedLineType,
+            description: maintenanceDescription,
+            frequency: maintenanceFrequency,
+            estimated_duration: estimatedDuration,
+            components_involved: componentsInvolved
+        }
+
+        //post request
+        axios.post("http://localhost:9000/api/maintenance-type", regMaintenanceType)
+            .then(response => {
+                console.log("Data sent successfully: ", response.data);
+                setSuccessMessage("Data sent successfully.");
+                setError("");
+
+                setTimeout(() => {
+                    setSuccessMessage("");
+                }, [10000])
+            })
+            .catch(error => {
+                console.error("Error sending data :", error);
+                setError("An error occurred while sending the data.");
+                setSuccessMessage("");
+            })
+    }
 
     return (
         <CContainer md>
@@ -59,16 +92,16 @@ const CreateMaintenanceType = () => {
                             </CFormSelect>
                         </CCol>
                         <CCol xs>
-                            <CFormInput label="Maintenance Type Name" value={maintenanceTypeName} placeholder="Maintenance Type Name" aria-label="Maintenance Type Name"/>
+                            <CFormInput label="Maintenance Type Name" value={maintenanceTypeName} onChange={(e) => setMaintenanceTypeName(e.target.value)} placeholder="Maintenance Type Name" aria-label="Maintenance Type Name"/>
                         </CCol>
                     </CRow>
                     <br />
                     <CRow>
                         <CCol xs>
-                            <CFormInput label="Frequency of Maintenance" value={maintenanceFrequency} placeholder="Frequency of Maintenance" aria-label="Frequency of Maintenance"/>
+                            <CFormInput label="Frequency of Maintenance" value={maintenanceFrequency} onChange={(e) => setMaintenanceFrequency(e.target.value)} placeholder="Frequency of Maintenance" aria-label="Frequency of Maintenance"/>
                         </CCol>
                         <CCol xs>
-                            <CFormInput label="Estimated Duration" value={estimatedDuration} placeholder="Estimated Duration" aria-label="Estimated Duration"/>
+                            <CFormInput label="Estimated Duration" value={estimatedDuration} onChange={(e) => setEstimatedDuration(e.target.value)} placeholder="Estimated Duration" aria-label="Estimated Duration"/>
                         </CCol>
                     </CRow>
                     <br />
@@ -79,6 +112,7 @@ const CreateMaintenanceType = () => {
                                 label="Description"
                                 rows={3}
                                 value={maintenanceDescription}
+                                onChange={(e) => setMaintenanceDescription(e.target.value)}
                             ></CFormTextarea>
                         </CCol>
                         <CCol xs>
@@ -87,18 +121,21 @@ const CreateMaintenanceType = () => {
                                 label="Components Involved"
                                 rows={3}
                                 value={componentsInvolved}
+                                onChange={(e) => setComponentsInvolved(e.target.value)}
                             ></CFormTextarea>
                         </CCol>
                     </CRow>
                     <br />
                     <CRow>
                         <CCol xs>
-                            <CButton color="primary">Save</CButton>
+                            <CButton color="primary" onClick={handleSave}>Save</CButton>
                         </CCol>
                     </CRow>
                 </CCardBody>
             </CCard>
             <br />
+            {error && <CAlert color="danger">{error}</CAlert>}
+            {successMessage && <CAlert color="success">{successMessage}</CAlert>}
         </CContainer>
     );
 };
